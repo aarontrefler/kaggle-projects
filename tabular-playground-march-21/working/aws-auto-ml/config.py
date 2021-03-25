0d6e16337dcc503dff24003455d917dda6a29d6a
Aaron Trefer <trefler.aaron@gmail.com>
2021-03-25 17:00:37 +0000

    EOD 3/25

---
+++ tabular-playground-march-21/working/aws-auto-ml/config.py
@@ -0,0 +1,10 @@
+# Local directories
+input_path = '/root/kaggle-projects/tabular-playground-march-21/input'
+
+# S3 directories
+s3_out_dir = 's3://briorep-s3-ds/Aaron-temp/Projects-Personal/kaggle-tabular-playground-march-21/'
+s3_autopilot_out_bucket = 'briorep-s3-ds'
+s3_autopilot_out_prefix = 'Aaron-temp/Projects-Personal/kaggle-tabular-playground-march-21/kaggle-tabular-march-21/sagemaker-automl-canditates/'
+
+# AWS Autopilot information
+endpoint_name = 'kaggle-23032021'

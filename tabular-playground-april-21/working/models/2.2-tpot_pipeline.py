59179ff3be06818c77695f7cd7391c85e3cbdd4f
Aaron Trefer <trefler.aaron@gmail.com>
2021-04-27 21:11:31 +0000

    Finalize project

--- tabular-playground-april-21/working/models/2.2-tpot_pipeline.py
+++ tabular-playground-april-21/working/models/2.2-tpot_pipeline.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,11 +12,13 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=3)
 
-# Average CV score on the training set was: 0.7816700000000001
-exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=3, max_features=0.7000000000000001, min_samples_leaf=16, min_samples_split=3, n_estimators=100, subsample=0.4)
-# Fix random state in exported estimator
-if hasattr(exported_pipeline, 'random_state'):
-    setattr(exported_pipeline, 'random_state', 3)
+# Average CV score on the training set was: 0.78197
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.01, max_depth=3, max_features=0.55, min_samples_leaf=10, min_samples_split=9, n_estimators=100, subsample=0.9500000000000001)),
+    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.7500000000000001, min_samples_leaf=19, min_samples_split=18, n_estimators=100)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 3)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
